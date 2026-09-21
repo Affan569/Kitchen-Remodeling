b38c2f11e20e8365db9bb316cd8cb7e2488aabb9
Affan569 <affanzahid612@gmail.com>
2026-09-21 13:22:13 -0700

    Clean slate: removed all app code and design files, keeping minimal setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,8 @@
-import React from "react";
-import { Header, Footer, ScrollWrapper } from "./components/layout";
-import {
-  Hero,
-  TrustBar,
-  Services,
-  BeforeAfter,
-  Projects,
-  Process,
-  Video,
-  WhyChooseUs,
-  Testimonials,
-  Finance,
-  FAQ,
-  Contact,
-} from "./components/sections";
-
 function App() {
   return (
-    <ScrollWrapper>
-      <div className="min-h-screen flex flex-col">
-        <Header />
-        <main className="flex-grow">
-          <Hero />
-          <TrustBar />
-          <Services />
-          <BeforeAfter />
-          <Projects />
-          <Process />
-          <Video />
-          <WhyChooseUs />
-          <Testimonials />
-          <Finance />
-          <FAQ />
-          <Contact />
-        </main>
-        <Footer />
-      </div>
-    </ScrollWrapper>
+    <div className="min-h-screen bg-white">
+      {/* Blank page */}
+    </div>
   );
 }
 
--- src/components/ui/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Button } from "./Button.jsx";
-export { default as Container } from "./Container.jsx";
-export { default as Section } from "./Section.jsx";
-export { default as Icon } from "./Icon.jsx";
-export { default as ScrollReveal } from "./ScrollReveal.jsx";
