46f4694f03d24d2e46668ca0b4b73a745da9bbd4
Affan569 <affanzahid612@gmail.com>
2026-09-22 13:50:01 -0700

    Aapke changes ka short message (jaise: updated styling and content)

--- src/components/sections/index.js
+++ src/components/sections/index.js
@@ -3,6 +3,6 @@ export { default as WhyChooseUs } from "./whychooseus.jsx";
 export { default as Projects } from "./projects.jsx";
 export { default as Reviews } from "./reviews.jsx";
 export { default as FAQ } from "./faq.jsx";
-export { default as ServiceCards } from "./servicecards.jsx";
+export { default as ServiceCards } from "./ServiceCards.jsx";
 export { default as ComprehensiveFeature } from "./comprehensivefeature.jsx";
 export { default as Cards } from "./cards.jsx";
